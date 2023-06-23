03295391d41269ad0848a045dbe0e780a12b3c49
Alokam Chinenye Augusta <alokamaugusta@gmail.com>
2023-06-23 15:18:17 +0000

    Update

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-import streamlit as stfrom predict_page import show_predict_page# page= st.sidebar.selectbox("Explore or Predict", ("Predict","Explore"))show_predict_page()
+import streamlit as st
+from predict_page import show_predict_page
+
+
+# page= st.sidebar.selectbox("Explore or Predict", ("Predict","Explore"))
+
+
+show_predict_page()
